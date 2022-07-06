199aef6b8a912ac9c9da9005585dcc2f8421b8a3
Christian Wilfried <44750483+christianwilfried31@users.noreply.github.com>
2022-07-06 02:51:01 +0200

    loop statments

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,14 @@
  */
 int main(void)
 {
-	_putchar('C');
-	_putchar('h');
-	_putchar('r');
-	_putchar('i');
-	_putchar('s');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
 	_putchar('t');
-	_putchar('i');
+	_putchar('c');
+	_putchar('h');
 	_putchar('a');
-	_putchar('n');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
