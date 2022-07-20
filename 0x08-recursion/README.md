40bd24071416e0364c5e5379efdd0d4e3979173b
Christian Wilfried <44750483+christianwilfried31@users.noreply.github.com>
2022-07-21 00:44:31 +0200

    recursion

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+(1) Scripts for recursion(2)
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,14 @@
+#ifndef DEFA547F_D436_474C_8906_0D803671524B
+#define DEFA547F_D436_474C_8906_0D803671524B
+int _putchar(char);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif /* DEFA547F_D436_474C_8906_0D803671524B */
